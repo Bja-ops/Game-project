c5c1ca5cc85cb54dc7cc3e25835fdd2844dd5922
Bartek <bartek.jawor@onet.eu>
2025-02-19 21:00:40 +0100

    Gamecode.cpp

--- Gamecode.cpp
+++ Gamecode.cpp
@@ -8,12 +8,13 @@ struct Character {
     int dexterity;
     int intelligence;
     int faith;
+    int health;
     string char_name;
     const int start_level;
     ClassType class_type;
 
-    Character(string name, int str, int dex, int intel, int fai, int level, ClassType c_type)
-        : char_name(name), strength(str), dexterity(dex), intelligence(intel), faith(fai), start_level(level), class_type(c_type) {}
+    Character(string name, int hp, int str, int dex, int intel, int fai, int level, ClassType c_type)
+        : char_name(name), health(hp), strength(str), dexterity(dex), intelligence(intel), faith(fai), start_level(level), class_type(c_type) {}
 
     void printClass()
     {
@@ -29,10 +30,10 @@ struct Character {
 };
 
 int main() {
-    Character hero1("Arthur", 12, 7, 5, 3, 1, WARRIOR);
-    Character hero2("Merlin", 4, 6, 15, 10, 1, MAGE);
-    Character hero3("Jarvis",4,10,17,20,1,PALADIN );
-    Character hero4("Robin", 10, 15, 8,6,1,ASSASIN);
+    Character hero1("Arthur", 12, 12, 7, 5, 3, 1, WARRIOR);
+    Character hero2("Merlin",6, 4, 6, 15, 10, 1, MAGE);
+    Character hero3("Jarvis",8,4,10,17,20,1,PALADIN );
+    Character hero4("Robin", 10,10, 15, 8,6,1,ASSASIN);
     hero1.printClass();
     hero2.printClass();
     hero3.printClass();
